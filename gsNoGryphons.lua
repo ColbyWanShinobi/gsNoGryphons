8e3085a10a1068843739d70de2f476d1aa73d108
Colby Butler <cbutler@CBUTLER-MAC.local>
2015-09-23 15:33:11 -0400

    Initial commit

---
+++ gsNoGryphons.lua
@@ -0,0 +1,16 @@
+--[[
+gsNoGryphons
+Created by ColbyWanShinobi
+email: colbywanshinobi@gameshaman.com
+web: gameshaman.com
+repo: https://github.com/ColbyWanShinobi/gsNoGryphons.git
+--]]
+
+--Hide the Left Gryphon
+MainMenuBarLeftEndCap:Hide()
+
+--Hide the Right Gryphon
+MainMenuBarRightEndCap:Hide()
+
+--Print a status message
+print("NoGryphons by GameShaman Loaded. Gryphons have been removed!")
