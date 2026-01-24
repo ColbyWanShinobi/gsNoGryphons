29ddc0474a6fb1d470cf58527fd902f6f71fda67
Colby Butler <colby@owljet.com>
2026-01-24 02:46:38 -0500

    UPdated for 12.0

--- gsNoGryphons.lua
+++ gsNoGryphons.lua
@@ -8,7 +8,13 @@ repo: https://github.com/ColbyWanShinobi/gsNoGryphons.git
 
 print("gsNoGryphons by gameshaman.com - Addon Loaded");
 
-if MainMenuBarLeftEndCap then
+-- Check for the latest structure first (Patch 12.0.0+)
+if MainActionBar and MainActionBar.EndCaps then
+    print("gsNoGryphons: Found Patch 12.0+ UI. Removing Gryphons...");
+
+    --Hide the EndCaps frame (contains both gryphons/wyverns)
+    MainActionBar.EndCaps:Hide();
+elseif MainMenuBarLeftEndCap then
     print("gsNoGryphons: Found Classic UI. Removing Gryphons...");
 
     --Hide the Left Gryphon
@@ -16,14 +22,28 @@ if MainMenuBarLeftEndCap then
 
     --Hide the Right Gryphon
     MainMenuBarRightEndCap:Hide();
-else
-    print("gsNoGryphons: Found Retail. Removing Gryphons...");
+elseif MainMenuBarArtFrame then
+    print("gsNoGryphons: Found Modern Retail UI. Removing Gryphons...");
+
+    --Hide the Left Gryphon
+    if MainMenuBarArtFrame.LeftEndCap then
+        MainMenuBarArtFrame.LeftEndCap:Hide();
+    end
+
+    --Hide the Right Gryphon
+    if MainMenuBarArtFrame.RightEndCap then
+        MainMenuBarArtFrame.RightEndCap:Hide();
+    end
+elseif MainMenuBar and MainMenuBar.EndCaps then
+    print("gsNoGryphons: Found Older Retail UI. Removing Gryphons...");
 
     --Hide the Left Gryphon
     MainMenuBar.EndCaps.LeftEndCap:Hide();
 
     --Hide the Right Gryphon
     MainMenuBar.EndCaps.RightEndCap:Hide();
+else
+    print("gsNoGryphons: Warning - Could not find gryphon UI elements. They may have been moved in this WoW version.");
 end
 
 --Print a status message
